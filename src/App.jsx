035314a419470b6d1ea137f4490d356adabe267b
Gwolo Julius <gwolojulius2@gmail.com>
2024-11-07 14:06:39 +0200

    recipie initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <p className="text-3xl font-bold underline">Hello world!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
